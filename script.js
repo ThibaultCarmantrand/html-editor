613d4b859efa3178ce42f69568e9cf4338fab46d
Thibault Carmantrand <t.carmantrand@openmailbox.org>
2017-02-17 17:48:35 +0100

    Added: main features

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 var save_btn = document.getElementById('save');
-var textarea = document.getElementById('textarea');
+var editor = document.getElementById('editor');
 var result = document.getElementById('result');
 
 save_btn.addEventListener('click', save);
 
 function save () {
   var new_text = '';
-  var text = textarea.value;
+  var text = editor.innerHTML;
 
   new_text = '<p>' + text + '</p>';
   console.log(new_text);
   result.innerHTML = new_text;
+}
+
+
+function command (name, argument) {
+  if (typeof argument === 'undefined') argument = '';
+  // Execute the command
+  switch(name) {
+    case 'createLink':
+      argument = prompt("Adresse du lien ?");
+    break;
+
+    case 'insertImage':
+      argument = prompt("Adresse de l'image ?");
+    break;
+  }
+  document.execCommand(name, false, argument);
 }
