b9b3107b1de7a8012dde3d9811a9ccf283e348a0
Margaux Girard <m.girard1988@gmail.com>
2017-02-18 17:23:48 +0100

    reinit id name

--- src/assets/js/script2.js
+++ src/assets/js/script2.js
@@ -5,11 +5,11 @@ var section_editable = editable.textContent;
 
 
 // attribute an action of buttons with class "command"
-var btn = document.querySelectorAll('.command'), i;
+var btn = document.querySelectorAll('button'), i;
 
 for (i=0; i<btn.length; ++i) {
   btn[i].addEventListener("click", function(){
-    var effect = this.id;
+    var effect = this.getAttribute('data-command');
     if (document.getSelection) {
       var selectedText = document.getSelection();
       var style_effect = document.execCommand(effect);
@@ -19,28 +19,11 @@ for (i=0; i<btn.length; ++i) {
   //btn[i].style.color="green";
 }
 
-// color text button
-//var foreColor = document.getElementById('control_txt-color');
-//foreColor.addEventListener('click', updateInput);
-
-/*function foreColor_button() {
-  if (document.getSelection) {
-      selectedText = document.getSelection();
-    //  var color_selected = document.getElementById("valueInput").value;
-      var foreColor_effect = document.execCommand('foreColor','','#'+color_selected+"'");
-      var res = section_editable.replace(selectedText,foreColor_effect);
-      selectedText.innerHTML = res;
-  }
-}
-*/
-
 function updateInput(evt) {
-  document.getElementById("valueInput").value=evt;
-  console.log(evt);
+  document.getElementById("control_txt-color").value=evt;
   if (document.getSelection) {
     var selectedText = document.getSelection();
     var style_effect = document.execCommand('foreColor','','#'+evt+"'");
     var res = section_editable.replace(selectedText,style_effect);
   }
 }
-
